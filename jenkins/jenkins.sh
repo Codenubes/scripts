547329050272990a4cfae2d35461637ca5d58c9f
Shethap <shethap@gmail.com>
2021-05-09 00:18:28 +0800

    Update jenkins.sh

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,2 +1,2 @@
 cd ~/war/WEB-INF/lib
-java -jar cli-2.277.2.jar -s http://127.0.0.1:8080/ install-plugin maven-plugin:3.10
+java -jar cli-2.277.2.jar -s http://127.0.0.1:8080/ install-plugin < /kodrex/jenkins/plugins.txt
