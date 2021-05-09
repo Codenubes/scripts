baf06db1965698e54ba7a8533260ca8cf1c737a7
Shethap <shethap@gmail.com>
2021-05-09 23:40:30 +0800

    updated

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/jenkins_home/war/WEB-INF
-chmod -R 755 /var/jenkins_home/war/WEB-INF
-java -jar jenkins-cli.jar  -s http://localhost:8080 install-plugin maven-plugin:3.10
-java -jar jenkins-cli.jar  -s http://localhost:8080 restart
+cd /var/jenkins_home/war/WEB-INF/lib/
+chmod -R 755 /var/jenkins_home/war/WEB-INF/lib
+java -jar  /var/jenkins_home/war/WEB-INF/lib/cli-2.277.4.jar  -s http://localhost:8080 install-plugin < /kodrex/jenkins/plugins.txt
+java -jar  /var/jenkins_home/war/WEB-INF/lib/cli-2.277.4.jar  -s http://localhost:8080 restart
