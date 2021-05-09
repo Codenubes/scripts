e4951ab1575a8bc451b4e81dd0907cad46c1293c
Shethap <shethap@gmail.com>
2021-05-10 01:27:25 +0800

    updated

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/install-plugins.sh < /kodrex/jenkins/plugins.txt
-java -jar  /var/jenkins_home/war/WEB-INF/lib/cli-2.291.jar  -s http://localhost:8080 restart
+/bin/jenkins-plugin-cli --verbose -f /kodrex/jenkins/plugins.txt
