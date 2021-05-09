2d9b24948cfb110fb6d7c680604e8ec72db60448
Shethap <shethap@gmail.com>
2021-05-09 18:01:54 +0800

    plugins updates via API

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,2 +1,17 @@
-cd ~/war/WEB-INF/lib
-java -jar cli-2.277.2.jar -s http://127.0.0.1:8080/ install-plugin < /kodrex/jenkins/plugins.txt
+#!/bin/bash
+
+host=http://localhost:8080
+pluginInstallationURL=/pluginManager/installNecessaryPlugins
+jenkinsRestartURL=/safeRestart
+
+# git plugin https://wiki.jenkins-ci.org/display/JENKINS/Git+Plugin
+curl -X POST -d '<jenkins><install plugin="git@2.0" /></jenkins>' --header 'Content-Type: text/xml' $host$pluginInstallationURL
+
+# artifactory plugin https://wiki.jenkins-ci.org/display/JENKINS/Artifactory+Plugin
+curl -X POST -d '<jenkins><install plugin="artifactory@2.2.1" /></jenkins>' --header 'Content-Type: text/xml' $host$pluginInstallationURL
+
+# wait 20 sec
+sleep 20
+
+# jenkins safe restart
+curl -X POST $host$jenkinsRestartURL
