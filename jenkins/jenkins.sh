ad6b405ed0309edbbc33d430e1e8397091d6ad1f
Shethap <shethap@gmail.com>
2021-05-09 23:13:05 +0800

    updated

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-host=http://localhost:8080
-pluginInstallationURL=/pluginManager/installNecessaryPlugins
-jenkinsRestartURL=/safeRestart
-
-# git plugin https://wiki.jenkins-ci.org/display/JENKINS/Git+Plugin
-curl -X POST -d '<jenkins><install plugin="git@2.0" /></jenkins>' --header 'Content-Type: text/xml' $host$pluginInstallationURL
-
-# artifactory plugin https://wiki.jenkins-ci.org/display/JENKINS/Artifactory+Plugin
-curl -X POST -d '<jenkins><install plugin="artifactory@2.2.1" /></jenkins>' --header 'Content-Type: text/xml' $host$pluginInstallationURL
-
-# wait 20 sec
-sleep 20
-
-# jenkins safe restart
-curl -X POST $host$jenkinsRestartURL
+cd /var/jenkins_home/war/WEB-INF
+java -jar jenkins-cli.jar  -s http://localhost:8080 install-plugin maven-plugin:3.10
+java -jar jenkins-cli.jar  -s http://localhost:8080 restart
