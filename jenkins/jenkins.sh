5db1d208ecc1ceaa8d769b2607618ef4d596b5cc
Shethap <shethap@gmail.com>
2021-05-10 21:45:48 +0800

    updated

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 /bin/jenkins-plugin-cli --verbose -d /var/jenkins_home/plugins -f /kodrex/jenkins/plugins.txt &> /kodrex/jenkins/error.txt
-http://localhost:8080/safeRestart
+curl http://localhost:8080/safeRestart
