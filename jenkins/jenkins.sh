bb18ac914c4ddebc5accde48564fc8b9b437afc3
Shethap <shethap@gmail.com>
2021-05-22 00:42:10 +0800

    updated

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 cp /kodrex/jenkins/casc.yaml /var/jenkins_home/
+wget -O /tmp/ https://apachemirror.sg.wuchna.com/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz
+tar -xvf /tmp/apache-maven-3.8.1-bin.tar.gz -C /var/jenkins_home/
 /bin/jenkins-plugin-cli --verbose -d /var/jenkins_home/plugins -f /kodrex/jenkins/plugins.txt &> /kodrex/jenkins/error.txt
 curl http://localhost:8080/safeRestart
